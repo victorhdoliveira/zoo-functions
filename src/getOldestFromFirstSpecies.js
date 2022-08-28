aaddf476c9f92856c49ef9e96265d346cda7a5be
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-28 19:59:09 -0300

    Requisito nº 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const work = employees.find((employee) => employee.id === id);
+  const animal = work.responsibleFor.find((firstAnimal) => firstAnimal);
+  const animalAge = species.filter((specie) => specie.id === animal)
+    .map((feature) => feature.residents).pop([]).sort((a, b) => b.age - a.age);
+  return Object.values(animalAge[0]);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.filter((employee) => employee.id === id)
+    .some((identify) => identify.id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+    || identify.id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+    || identify.id === '0e7b460e-acf4-4e17-bcb3-ee472265db83');
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
