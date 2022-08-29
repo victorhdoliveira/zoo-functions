3fd0544fa22f6047fd6f69eab42309be4dacd38b
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-29 10:10:03 -0300

    Requisito nº 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -9,7 +9,12 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => employee.managers
+    .includes(managerId))
+    .map((subordinate) => `${subordinate.firstName} ${subordinate.lastName}`);
 }
 
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
