81b70df73ac4ea81290535e8754a55a46ed22fb2
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-29 16:53:51 -0300

    Requisito nº 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verificando se HandlerElephants é de fato uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('Verificando se o parametro da função é uma string', () => {
+    expect(handlerElephants(15)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verificando se há um elefante chamado Orval', () => {
+    expect(handlerElephants('names')).toContain('Orval');
+  });
+  it('Verificando se há quatro elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Verificando se a média de idade está correta', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
