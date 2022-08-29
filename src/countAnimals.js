db6d7666cb761d3451e5ff8358c82894301cff2b
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-29 14:46:00 -0300

    Requisito nº 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const AnimalsList = {};
+    species.forEach((specie) => { AnimalsList[specie.name] = specie.residents.length; });
+    return AnimalsList;
+  }
+  if (animal.sex === undefined) {
+    return species.find((specie) => specie.name === animal.specie)
+      .residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie)
+    .residents.filter((genre) => genre.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
