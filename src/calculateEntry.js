451653155043fb3b7d140c9b4d6cf63fa191db6e
Victor Hugo <victorhdoliveira@gmail.com>
2022-08-30 11:27:32 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const ageGroup = entrants.map((entrant) => entrant.age);
+  const kid = ageGroup.filter((age) => age < 18).length;
+  const youngAdult = ageGroup.filter((age) => age >= 18 && age < 50).length;
+  const aged = ageGroup.filter((age) => age >= 50).length;
+  return { child: kid, adult: youngAdult, senior: aged };
+}
 
+function payment(entrants) {
+  return (Object.values(countEntrants(entrants))[0] * prices.child
+  + Object.values(countEntrants(entrants))[1] * prices.adult
+  + Object.values(countEntrants(entrants))[2] * prices.senior);
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui //
+  if (entrants === undefined || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  return payment(entrants);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -9,6 +9,4 @@ function getOldestFromFirstSpecies(id) {
   return Object.values(animalAge[0]);
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-
 module.exports = getOldestFromFirstSpecies;
